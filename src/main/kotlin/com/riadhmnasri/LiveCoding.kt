13a4c1283494d96d91a6027c22cadf2f463fd80d
Riadh MNASRI <riadh.mnasri@gmail.com>
2022-11-01 19:45:23 +0100

    Functional Programming With Kotlin and Arrow.KT - iteration 4 - restart

--- src/main/kotlin/com/riadhmnasri/LiveCoding.kt
+++ src/main/kotlin/com/riadhmnasri/LiveCoding.kt
@@ -4,68 +4,41 @@ import arrow.core.*
 
 fun main() {
     // First citizen functions
-    // Declare Function and set it to variable
-    val isEven: (Int) -> Boolean = { n -> n % 2 == 0 }
-    println(isEven(44))
+    // Declare Function and set it to variable // isEven
 
     // Pass function numberPredicate to function
     val numbers = listOf(7, 9, 8, 5, 4, 8)
-    println(numbers.filter { n -> numberPredicate(n) { it % 2 == 0 } })
 
     // Lazy behaviour
     // By and Lazy keyword
-    val message by lazy {
-        "Hello Kotlin"
-    }
 
     // Show Lazy Behaviour
-    val lazyEval = listOf({ 8 + 2 }, { 7 * 6 }, { 9 / 0 }, { 6 - 5 }).asReversed()
-    println(lazyEval)
 
     // Monad : Option fast Demo (map function)
-    val messageOpt = "Hello".toOption()
-    println(messageOpt.map(String::uppercase).getOrElse { "Default" })
 
     // Either: Deep Demo
     // Pattern Matching
     val result: Either<MyError, Int> = Either.Right(1)
     // Pattern Matching
-    println(
-        when (result) {
-            is Either.Left -> -1
-            is Either.Right -> result.value
-        }
-    )
+
 
     // Function Fold
 
     // Either - GetOrElse: addresses Left case
     val error = MyError("Custom Error")
     val result2: Either<MyError, Int> = Either.Left(error)
-    println(result2.getOrElse { "Default" })
-
 
     // Map addresses Right case (transform right value if found else do nothing): Right biased
     // Either - flatMap/map - pattern
     val result4: Either<MyError, Int> = Either.Right(1)
     val result5: Either<MyError, Int> = Either.Right(2)
-    println(
-        result4.flatMap { x ->
-            result5.map { y ->
-                x + y
-            }
-        }.getOrElse { "" }
-    )
 
     // Validated NonEmptyList/invalidNel/flatten
     val error1 = MyError("error 1")
     val error2 = MyError("error 2")
-    val test = listOf(error1, error2).invalidNel()
-    println(test.fold({ it }, { it }).flatten())
 
     // Monoid - fold list
-    val list = listOf("Functional ", "Programming ", "With ", "Kotlin ")
-    println(list.fold("Talk: ") { a, b -> a + b })
+
 
 }
 
